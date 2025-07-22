629b0515a178e8d27123269579d4b47aa731026a
Jordan Trask <jtrask@gmail.com>
2025-07-22 13:11:15 -0400

    Small fixes

--- goaccess/enhance-goaccess.sh
+++ goaccess/enhance-goaccess.sh
@@ -210,7 +210,7 @@ generate_historical_report_site() {
     declare -A SEEN_DATES=()
     for archive in "${FILE_ARCHIVES[@]}"; do
         _running3 "Processing archive: $archive"
-        [[ ! -f $archive ]] && { continue; _running3 "File not found: $archive"; }
+        [[ ! -f $archive ]] && { _running3 "File not found: $archive"; continue;  }
         filename=$(basename "$archive")
         temp="${filename#${SITE_ID}.log-}"
         temp="${temp%.gz}"
